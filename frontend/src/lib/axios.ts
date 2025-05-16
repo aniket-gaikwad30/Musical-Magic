9e6d3e0259cfa9738f9c802b05ffc58d12c2407d
Aniket Gaikwad <aniketgaikwadhere30@gmail.com>
2025-05-16 08:06:18 +0530

    finish 8

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,5 +2,8 @@ import axios from "axios";
 
 export const axiosInstence = axios.create({
   baseURL:
-    import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : "/api",
 });
+
