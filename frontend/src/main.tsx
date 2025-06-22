47692020405ddf32130c8153cba84e3e5cdfc8af
Aniket Gaikwad <aniketgaikwadhere30@gmail.com>
2025-06-22 15:21:42 +0530

    Fix: Set Clerk redirect URLs

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -14,7 +14,12 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+		<ClerkProvider
+			publishableKey={PUBLISHABLE_KEY}
+			afterSignInUrl='/'
+			afterSignUpUrl='/'
+			afterSignOutUrl='/'
+		>
 			<AuthProvider>
 				<BrowserRouter>
 					<App />
