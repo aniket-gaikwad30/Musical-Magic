c1e731ad5ae2187e1be1d648ae3ecf3ee54799ec
Aniket Gaikwad <aniketgaikwadhere30@gmail.com>
2025-06-22 14:01:52 +0530

    some changes and errors solved

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -1,8 +1,20 @@
+import { Webhook } from "svix";
 import { User } from "../models/user.model.js";
 
 export const authCallback = async (req, res, next) => {
 	try {
-		const { id, firstName, lastName, imageUrl } = req.body;
+		const payload = req.body;
+		const headers = req.headers;
+
+		const wh = new Webhook(process.env.WEBHOOK_SECRET);
+		let msg;
+		try {
+			msg = wh.verify(payload, headers);
+		} catch (err) {
+			return res.status(400).json({});
+		}
+
+		const { id, first_name, last_name, image_url } = msg.data;
 
 		// check if user already exists
 		const user = await User.findOne({ clerkId: id });
@@ -11,8 +23,8 @@ export const authCallback = async (req, res, next) => {
 			// signup
 			await User.create({
 				clerkId: id,
-				fullName: `${firstName || ""} ${lastName || ""}`.trim(),
-				imageUrl,
+				fullName: `${first_name || ""} ${last_name || ""}`.trim(),
+				imageUrl: image_url,
 			});
 		}
 
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,8 @@
-import { Router } from "express";
+import { Router, raw } from "express";
 import { authCallback } from "../controller/auth.controller.js";
 
 const router = Router();
 
-router.post("/callback", authCallback);
+router.post("/callback", raw({ type: "application/json" }), authCallback);
 
 export default router;
